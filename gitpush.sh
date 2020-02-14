8a368dfdcba5f0d856a2934b31cb187f6f7ecb0f
umangandrewfrancis <umangandrewfrancis@gmail.com>
2020-02-14 15:09:11 +0000

    new commit

---
+++ gitpush.sh
@@ -0,0 +1,17 @@
+git config --global user.email "umangandrewfrancis@gmail.com"
+
+git config --global user.name "umangandrewfrancis"
+
+
+
+git init /home/site/wwwroot
+
+cd /home/site/wwwroot ; git add .
+
+cd /home/site/wwwroot ; git commit -m "new commit"
+
+cd /home/site/wwwroot ; git remote add origin https://umangandrewfrancis:andrewassassin32@github.com/UmangAndrewFrancis/backup_flask1.git
+
+cd /home/site/wwwroot ; git push prigin master
+
+
